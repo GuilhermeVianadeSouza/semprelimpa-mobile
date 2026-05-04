e75235def29ea6fb3986e821b185002071355f21
GUILHERME VIANA DE SOUZA <25132756@dsn127m1246728.cfp127aluno.local>
2026-05-04 11:21:29 -0300

    feat: Validação de cpf enviado conforme regra de negócio concluido.

--- src/utils/validacoes.ts
+++ src/utils/validacoes.ts
@@ -40,3 +40,32 @@ export const validarTelefone = (telefoneStr: string): boolean => {
 
     return true
 }
+
+export const validarCpf = (cpfStr: string): boolean => {
+    const cpf = cpfStr.replace(/\D/g, '')
+
+    if (cpf.length !== 11) return false
+
+    if(/^(\d)\1+$/.test(cpf)) return false
+
+    let soma = 0
+    let resto
+
+    for (let i = 1; i <=9; i++){
+        soma = soma + parseInt(cpf.substring(i - 1, i)) * (11 - i)
+    }
+    resto = (soma * 10) % 11
+    if (resto === 10 || resto === 11) resto = 0
+    if (resto !== parseInt(cpf.substring(10,11))) return false
+    
+    soma = 0
+
+    for (let i = 1; i <= 10; i++){
+        soma = soma + parseInt(cpf.substring(i - 1, i)) * (12 - i)
+    }
+    resto = (soma * 10) % 11
+    if (resto === 10 || resto === 11) resto = 0
+    if (resto !== parseInt(cpf.substring(10,11))) return false
+
+    return true
+}
